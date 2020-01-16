411d181cc02394994bb883aff11b91699b496ec5
Igor Clemente <igorelectrofest2011@hotmail.com>
2020-01-16 03:01:22 -0300

    OMNISTACK10_CHALLENGE2_CREATEREACTAPP_CREATED_16012020

---
+++ omni2/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./App.css";
+
+import "./global.css";
+
+function App() {
+  return (
+    <div id="app">
+      <aside></aside>
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
